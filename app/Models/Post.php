9b6d5e7426e6b8d441a586a8d784fee16dfcd1da
michael-gk <michael@gestionkronos.com>
2023-08-04 16:36:44 -0400

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Post extends Model
 {
@@ -20,6 +21,21 @@ class Post extends Model
 
     // makes sthe following fields fillable
     // protected $fillable = ['title'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query
+                ->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+    
+        $query->when($filters['category'] ?? false, fn($query, $category) =>
+            $query->whereHas('category', fn ($query) => $query->where('slug', $category))
+        );
+        
+    }
+    
 
 
     public function getRouteKeyName()
@@ -38,6 +54,4 @@ class Post extends Model
         // Eloquent relationships: hasOne, hasMany, belongsTo, belongsToMany
         return $this->belongsTo(User::class, 'user_id');
     }
-
-
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
-
 
 
 /*
@@ -18,39 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', function () {
-    // to look up sql queries performed when the page loads
-    // \Illuminate\Support\Facades\DB::listen(function ($query) {
-    //     logger($query->sql, $query->bindings);
-    // });
-    
-    // as soon as we perform a query, get the results
-    return view('posts', [
-        'posts' => Post::latest()->get(),
-        'categories' => Category::all()
-    ]);
-
-})->name('home');
-
+Route::get('/', [PostController::class, 'index'])->name('home');
 
 // route binding key to an eloquent model, wild card name has to match the function parameter
 // inject models instances into your routes and controllers, automatically find the id specified in route url
 // 'post/{post:slug}' if you want to find the slug that match the route parameter Post::where('slug', $post)->firstOrFail()
-Route::get('posts/{post}', function (Post $post) {
-    return view('post', [
-        'post' => $post
-    ]);
-    
-});
-
-Route::get('categories/{category:slug}', function (Category $category) {
-    return view('posts', [
-        'posts' => $category->posts,
-        'currentCategory' => $category,
-        'categories' => Category::all()
-    ]);
-})->name('category');
+Route::get('posts/{post}', [PostController::class, 'show']);
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
